35891772391d122a1935d347bb6a8f7922de7918
Uwe Fechner <uwe.fechner.msc@gmail.com>
2022-10-30 15:06:01 +0100

    Add LibSerialPort

--- src/logger.jl
+++ src/logger.jl
@@ -1,4 +1,23 @@
 # Julia service to log incoming serial data to a file
 # create a new file daily
 
-const port = "/dev"
+using LibSerialPort
+
+const port = "/dev/ttyACM0"
+const baud = 9600
+
+function kill()
+    try
+        return readlines(`fuser -k /dev/ttyACM0`)
+    catch e
+        return nothing
+    end
+end
+
+# fuser -k /dev/ttyACM0
+
+sp = LibSerialPort.open(port, 9600)
+set_flow_control(sp)
+line=Base.readline(sp)
+println(line)
+close(sp)
